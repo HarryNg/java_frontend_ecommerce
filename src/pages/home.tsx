032f43244ecbc78e8e812116d07fe1899a113605
AA7776 <aa7776@student.jamk.fi>
2024-09-02 01:26:22 +0300

    Refactor: decoupling home page code

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,7 @@
 import { useCreateProduct, useDeleteProduct, useGetProducts } from "@/features/use-products"
 import { ProductList } from "@/components/product-list"
 import { ProductForm } from "@/components/product-form"
+import { ProductCreate } from "@/types";
 
 export function Home() {
   const {products , error: fetchError, isLoading: fetchLoading } = useGetProducts();
@@ -9,35 +10,37 @@ export function Home() {
 
   if (fetchLoading) return <p>Loading products...</p>;
   if (fetchError) return <p>Error fetching products: {fetchError.message}</p>;
+  const handleAddProduct = (product : ProductCreate) => {
+    createProductMutation.mutate(product, {
+      onSuccess: (data) => {
+        console.log("Product created successfully:", data);
+      },
+      onError: (error) => {
+        console.error("Error creating product:", error);
+      }
+    });
+  }
+  const handleDeleteProduct = (id: string) => {
+    deleteProductMutation.mutate(id, {
+      onSuccess: (data) => {
+        console.log("Product deleted successfully:", data);
+      },
+      onError: (error) => {
+        console.error("Error deleting product:", error);
+      }
+    });
+  }
 
   return (
     <div className="flex flex-col justify-center items-center gap-10">
       <h1 className="text-2xl">Welcome!</h1>
       <ProductForm 
-        onAddProduct = {(product) => {
-          createProductMutation.mutate(product, {
-            onSuccess: (data) => {
-              console.log("Product created successfully:", data);
-            },
-            onError: (error) => {
-              console.error("Error creating product:", error);
-            }
-          });
-        }}
+        onAddProduct = {handleAddProduct}
       />
       
       <ProductList 
         products={products} 
-        deleteProduct={(id) => {
-          deleteProductMutation.mutate(id, {
-            onSuccess: (data) => {
-              console.log("Product deleted successfully:", data);
-            },
-            onError: (error) => {
-              console.error("Error deleting product:", error);
-            }
-          });
-        }}
+        deleteProduct={handleDeleteProduct}
       />
 
 
