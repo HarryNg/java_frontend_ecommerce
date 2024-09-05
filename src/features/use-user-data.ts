0b09fa20acad9f94e7217ad290dc4b73a5dcba8f
AA7776 <aa7776@student.jamk.fi>
2024-09-05 05:19:41 +0300

    Refactor: form data

--- src/features/use-user-data.ts
+++ src/features/use-user-data.ts
@@ -32,12 +32,12 @@ export function useUserData() {
     if (typeof user !== 'string' && user) {
       setFormData({
         user_id: user.id || '',
-        email: user.email || '',
-        password: user.password || '',
-        firstName: user.firstName || '',
-        lastName: user.lastName || '',
-        address: user.address || '',
-        phoneNumber: user.phoneNumber || '',
+        email: user.email ,
+        password: user.password ,
+        firstName: user.firstName ,
+        lastName: user.lastName ,
+        address: user.address ,
+        phoneNumber: user.phoneNumber ,
         birthDate: user.birthDate || '23-03-1993',
         avatar: user.avatar || '',
       });
